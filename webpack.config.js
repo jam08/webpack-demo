789bef900130e553d2993076cd5496da476d6600
Janaina Marana <janainamarana@Janainas-MacBook-Pro.local>
2018-04-18 14:09:20 +0200

    added error-overlay-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    // Display only errors
+    stats: "errors-only",
+    host: process.env.HOST, // Defaults to localhost
+    port: process.env.PORT, // Defaults to 8080
+    open: true // Opens the page in browser
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack demo",
     }),
+    new ErrorOverlayPlugin()
   ],
 };
